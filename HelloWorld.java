28c88ba0de1f4aceb4996c452c93c1c754bc7103
molu92 <107424474+molu92@users.noreply.github.com>
2026-09-07 15:23:08 +0530

    Update output message in HelloWorld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package javaProgramsWithGit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World and Sumit");
 	}
 
 }
